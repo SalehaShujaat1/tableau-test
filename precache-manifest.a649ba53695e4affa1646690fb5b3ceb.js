5984d1b0b8080a7557e739d8f64963039ecb5584
Saleha Shujaat <saleha.shujaat@bazaar-app.com>
2022-05-24 17:22:02 +0500

    Updates

---
+++ precache-manifest.a649ba53695e4affa1646690fb5b3ceb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "74ddf6d29f8d54a6e5e0",
+    "url": "/static/css/main.251aab27.chunk.css"
+  },
+  {
+    "revision": "74ddf6d29f8d54a6e5e0",
+    "url": "/static/js/main.74ddf6d2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "1d19de1e847396599396",
+    "url": "/static/js/2.1d19de1e.chunk.js"
+  },
+  {
+    "revision": "f5a069d24c7855d24188c14f570b6a57",
+    "url": "/static/media/github.f5a069d2.svg"
+  },
+  {
+    "revision": "a67fae2d2dfb72fc7d6df6febfee10dc",
+    "url": "/index.html"
+  }
+];
